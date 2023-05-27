eac3bc2b2ca5b1553eccea2bc29f5084fb093ee1
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-05-27 16:26:29 +0530

    task completed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,8 @@ import "./Card.css"
 const Card = (props) => {
   return (
     <div className='card'>
-        <img src={props.avatar}/>
-        <p>{props.fname} {props.lname}</p>
+        <img src={props.avatar} alt='profile'/>
+        <p style={{fontWeight:"bold",color:"blue"}}>{props.fname} {props.lname}</p>
         <p>{props.email}</p>
         
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,42 @@
 import axios from 'axios';
 import "./Home.css";
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import Card from '../../components/Card/Card';
+import Logo from "../../assests/koach.jpg"
+import { Button } from '@mui/material';
+import BeatLoader from "react-spinners/BeatLoader";
 
 const Home = () => {
 
     const [data,setData]=useState([]);
-    useEffect(()=>{
+    let [loading, setLoading] = useState(false);
+
+    const getUsers=()=>{
+      setLoading(true)
        axios.get("https://reqres.in/api/users?page=1")
        .then((res)=>{
         console.log(res.data.data);
         setData(res.data.data)
+        setLoading(false)
        })
        
-    },[])
+    };
 
    
   return (
     <div className='home'>
-       <div className="cardsSec">
+      <div className='mainHead'>
+        <img src={Logo} alt='logo'/>
+        <Button variant='outlined' onClick={getUsers}>Get Users</Button>
+      </div>
+      <div className={loading ? "loading" : "hide"}>
+          <BeatLoader
+            color={'black'}
+            loading={loading}
+            size={15}
+          />
+        </div>
+       <div className={loading?"hide":"cardsSec"} >
          {
           data.map((item,key)=>{
               return(<Card key={key} email={item.email} avatar={item.avatar} fname={item.first_name} lname={item.last_name}/>)
